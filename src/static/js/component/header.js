832ca7b3d7f7a885438b5bece466aab5518e98d4
cathy.yang@jinfuzi.com <cathy.yang@jinfuzi.com>
2017-05-26 18:00:02 +0800

    add component

---
+++ src/static/js/component/header.js
@@ -0,0 +1,11 @@
+import 'static/css/component/header.css'
+import temp from 'components/header'
+
+function render () {
+	const html = temp({text: 'I am header'})
+	return html
+}
+
+export default render
+
+	
--- src/static/js/services/page-1.js
+++ src/static/js/services/page-1.js
@@ -1,6 +1,7 @@
 
 import 'static/css/page/page-1'
 import render from 'views/page-1'
+import header from 'static/js/component/header'
 
 $util.setTitle('page one')
 
@@ -8,7 +9,10 @@ const data = {
 	list: ['item 1', 'item 2', 'item 3']
 }
 
+const headerHtml = header()
+
 const html = render(data)
-$('#app').html(html)
+$('#app').html(headerHtml)
+$('#app').append(html)
 
 	
